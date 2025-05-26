e85418e53d4344d786eead702a080a167090bedc
AndreMaia95 <andrefsm95@gmail.com>
2025-05-26 18:00:40 +0100

    feat add : routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -45,6 +45,32 @@ const routes: Routes = [
         (m) => m.ExerciseDetailsPageModule
       ),
   },
+  {
+    path: 'tabs',
+    loadChildren: () => import('./tabs/tabs.module').then( m => m.TabsPageModule)
+  },
+  {
+    path: 'home',
+    redirectTo: 'tabs/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'profile',
+    loadChildren: () => import('./pages/profile/profile.module').then( m => m.ProfilePageModule)
+  },
+  {
+    path: 'statistics',
+    loadChildren: () => import('./pages/statistics/statistics.module').then( m => m.StatisticsPageModule)
+  },
+  {
+    path: 'saved',
+    loadChildren: () => import('./pages/saved/saved.module').then( m => m.SavedPageModule)
+  },
+  {
+    path: 'search',
+    loadChildren: () => import('./pages/search/search.module').then( m => m.SearchPageModule)
+  },
+
 ];
 
 @NgModule({
---
+++ src/app/pages/saved/saved.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-saved',
+  templateUrl: './saved.page.html',
+  styleUrls: ['./saved.page.scss'],
+  standalone: false
+})
+export class SavedPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
