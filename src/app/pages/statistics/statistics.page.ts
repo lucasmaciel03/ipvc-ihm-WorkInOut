58bc56ca9fc87701932ffe9a068cb397fb17f810
pedrocarvalho131002 <163552278+pedrocarvalho131002@users.noreply.github.com>
2025-05-27 19:00:53 +0100

    Stats Feito
    Stats feito

--- src/app/pages/statistics/statistics.page.ts
+++ src/app/pages/statistics/statistics.page.ts
@@ -1,16 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Workout {
+  title: string;
+  duration: string;
+  reps: string;
+  calories: number;
+  heartRate: number;
+  bpm: number;
+  date: Date;
+}
+
 @Component({
   selector: 'app-statistics',
-  templateUrl: './statistics.page.html',
-  styleUrls: ['./statistics.page.scss'],
-  standalone: false
+  templateUrl: 'statistics.page.html',
+  styleUrls: ['statistics.page.scss'],
+  standalone: false,
 })
 export class StatisticsPage implements OnInit {
+  selectedMetric: 'calories' | 'heart' | 'bpm' = 'calories';
 
-  constructor() { }
+  recentWorkouts: Workout[] = [
+    {
+      title: 'Chest Program',
+      duration: '12min',
+      reps: '3 x 12 reps',
+      calories: 802,
+      heartRate: 99,
+      bpm: 18,
+      date: new Date('2024-03-15'),
+    },
+    {
+      title: 'Arms Program',
+      duration: '12min',
+      reps: '3 x 12 reps',
+      calories: 802,
+      heartRate: 99,
+      bpm: 18,
+      date: new Date('2024-03-18'),
+    },
+  ];
 
-  ngOnInit() {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
