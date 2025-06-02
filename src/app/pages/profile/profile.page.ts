58a55bc9dd7c601626bb75ff7cec36d6c2501bec
pedrocarvalho131002 <163552278+pedrocarvalho131002@users.noreply.github.com>
2025-06-02 20:47:51 +0100

    Profile feito

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -1,16 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+interface Workout {
+  title: string;
+  date: string;        // ISO
+  duration: string;
+  reps: string;
+  calories: number;
+  heartRate: number;
+  bpm: number;
+}
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.page.html',
   styleUrls: ['./profile.page.scss'],
-  standalone: false
+  standalone: false,
 })
-export class ProfilePage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class ProfilePage {
+  workouts: Workout[] = [
+    {
+      title: 'Bicep Program',
+      date: '2024-03-15',
+      duration: '12min',
+      reps: '3 x 12 reps',
+      calories: 802,
+      heartRate: 99,
+      bpm: 18,
+    },
+    {
+      title: 'Bicep Program',
+      date: '2024-03-04',
+      duration: '32min',
+      reps: '3 × 12 reps',
+      calories: 802,
+      heartRate: 99,
+      bpm: 18,
+    },
+    {
+      title: 'Bicep Program',
+      date: '2024-03-04',
+      duration: '32min',
+      reps: '3 × 12 reps',
+      calories: 802,
+      heartRate: 99,
+      bpm: 18,
+    },
+  ];
 }
